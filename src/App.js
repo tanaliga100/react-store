8f40cd8da2936de98643a69c95644d26569990c8
tanaliga100 <jordantanaliga@gmail.com>
2023-03-28 02:16:33 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Footer, Navbar, Sidebar } from "./components";
+import {
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages";
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
+          <Route
+            path="checkout"
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path="error" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CartProvider } from "./context/cart_context";
+import { FilterProvider } from "./context/filter_context";
+import { ProductsProvider } from "./context/products_context";
+import { UserProvider } from "./context/user_context";
+import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
